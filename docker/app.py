97e9fa656fee3ac26c52d88e2bd517f05ce6159d
vamshii7 <vamshi9849@live.com>
2025-11-21 16:33:13 +0530

    added manifest

--- docker/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, jsonify
-app = Flask(__name__)
-
-@app.route('/metrics-json')
-def metrics_json():
-    return jsonify(cpu=0.42, memory=256)
-
-app.run(host='0.0.0.0', port=8080)
